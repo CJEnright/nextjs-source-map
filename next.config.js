f2e2b27b714080be07c1034d22ecc40da7c4a968
CJ Enright <cj@cjen.io>
2023-11-24 12:45:05 -0600

    Try setting webpack devtool

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.devtool = "eval-source-map";
+    }
+
+    return config;
+  },
+
   experimental: {
     serverSourceMaps: true,
   },

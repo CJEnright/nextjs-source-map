ba5a95f68c4bcf1a468e3e882b2fed5bb201e4c6
CJ Enright <cj@cjen.io>
2023-11-24 12:47:01 -0600

    Use source-map-support

--- src/app/test-app/route.ts
+++ src/app/test-app/route.ts
@@ -1,5 +1,7 @@
 export const dynamic = "force-dynamic";
 
+require("source-map-support").install();
+
 export async function GET() {
   console.log(new Error().stack);
 
--- src/pages/api/test-pages.ts
+++ src/pages/api/test-pages.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+require("source-map-support").install();
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(new Error().stack);
 
